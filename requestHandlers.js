6c6a458bc023eda64a1116c585dff9eaae1dae38
pupadupa <k.kabanov@gmail.com>
2013-05-19 21:30:34 +0400

    added upload and show image functionality

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,7 @@
 var exec = require("child_process").exec;
 var querystring = require("querystring");
 var fs = require("fs");
+var formidable = require("formidable");
 
 function start(response, postData){
     console.log("start handler called");
@@ -34,11 +35,25 @@ function start(response, postData){
         response.end();
 }
 
-function upload(response, postData){
+function upload(response, request){
     console.log("upload handler called");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You have send POST data: " + querystring.parse(postData).text);
-    response.end();
+    //response.writeHead(200, {"Content-Type": "text/plain"});
+    //response.write("You have send POST data: " + querystring.parse(postData).text);
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+        fs.rename(files.upload.path, "./tmp/test.png", function(err) {
+            if (err) {
+                fs.unlink("./tmp/test.png");
+                fs.rename(files.upload.path, "./tmp/test.png");
+            }
+        });
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
 
--- router.js
+++ router.js
@@ -1,7 +1,7 @@
-function route(handle, pathname, response, postData){
+function route(handle, pathname, response, request){
     console.log("About to route: "+pathname);
     if (typeof(handle[pathname]) === 'function') {
-       return handle[pathname](response, postData); 
+       return handle[pathname](response, request); 
     }else{
         console.log('no handler for: ' + pathname);
         response.writeHead(404, {"Content-Type": "text/plain"});
